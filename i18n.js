46dc344bae690eb47cebe0d80372c2eccbc7648d
boruihu <boruihu@mail.ustc.edu.cn>
2026-06-25 20:34:09 +0800

    add 我 after 九岁时 in Chinese About text

--- i18n.js
+++ i18n.js
@@ -69,7 +69,7 @@ const i18n = {
       header: "关于我",
       para1: "我的方向是基础数学。",
       para2:
-        '我出生于<a href="https://en.wikipedia.org/wiki/Hunan">湖南</a><a href="https://en.wikipedia.org/wiki/Changsha">长沙</a>，祖籍在<a href="https://en.wikipedia.org/wiki/Li_County,_Hunan">澧县</a>。九岁时和家人搬到<a href="https://en.wikipedia.org/wiki/Guangxi">广西</a><a href="https://en.wikipedia.org/wiki/Guilin">桂林</a>，在那里一直住到了十七岁。两年前，我考上了科大，所以我现在住在合肥。此外，我还在宁波、杭州和上海各住过几周，皆是难忘的回忆。',
+        '我出生于<a href="https://en.wikipedia.org/wiki/Hunan">湖南</a><a href="https://en.wikipedia.org/wiki/Changsha">长沙</a>，祖籍在<a href="https://en.wikipedia.org/wiki/Li_County,_Hunan">澧县</a>。九岁时，我和家人搬到<a href="https://en.wikipedia.org/wiki/Guangxi">广西</a><a href="https://en.wikipedia.org/wiki/Guilin">桂林</a>，在那里一直住到了十七岁。两年前，我考上了科大，所以我现在住在合肥。此外，我还在宁波、杭州和上海各住过几周，皆是难忘的回忆。',
       para3: "闲来无事的时候，我经常熬夜看曼城的比赛。我还非常喜欢去音乐节，与我喜爱的乐队们相会。我对各种语言的现代诗歌也很感兴趣。",
     },
     research: {
